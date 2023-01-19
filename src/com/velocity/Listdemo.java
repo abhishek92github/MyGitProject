cb019b930e56f3d1d58acd0b2e217f71799a4747
abhishek <abhishekkumarssm92@gmail.com>
2023-01-19 19:28:09 +0530

    new update

--- src/com/velocity/Listdemo.java
+++ src/com/velocity/Listdemo.java
@@ -1,17 +1,26 @@
 package com.velocity;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Listdemo {
 	
+	
+	
+
+
 	public static void main(String[] args) {
+		int [] a = {1,4,2,6};
 		
 		List<Integer> list= new	ArrayList<Integer>();
 		list.add(35);
 		list.add(25);
 		list.add(15);
 		list.add(12);
-		System.out.println(list);
+	
+		List list1 = Arrays.asList(a);
+		System.out.println(Arrays.toString(a));
+		System.out.println(list1);
 		
 		for(Integer l : list) {
 			System.out.print(" "+l);
