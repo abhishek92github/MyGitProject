cbfbb107f1106b40a46918ac7d07e5e173d867cd
abhishek <abhishekkumarssm92@gmail.com>
2023-01-19 14:54:29 +0530

    new ListDemo class Added

---
+++ src/com/velocity/Listdemo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Listdemo {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list= new	ArrayList<Integer>();
+		list.add(35);
+		list.add(25);
+		list.add(15);
+		list.add(12);
+		System.out.println(list);
+	}
+
+}
